54880f7a6d75b161ea2e5d544c1b964d064d92d5
Warren Smith <warren.lee.smith@hotmail.co.uk>
2024-10-21 21:37:10 +0000

    UPDATE: Enquiry views and templates

--- contactus/views.py
+++ contactus/views.py
@@ -3,29 +3,58 @@ from django.contrib import messages
 from .models import UserProfile, Enquiry
 from .forms import EnquiryForm
 
+
 def enquiries(request):
     """ A view to show all enquiries, including sorting and search queries """
-    profile = get_object_or_404(UserProfile, user=request.user)
+    
+    if request.user.is_authenticated:
+        profile = get_object_or_404(UserProfile, user=request.user)
+        is_anonymous = False
+    else:
+        profile = None
+        is_anonymous = True
 
     if request.method == 'POST':
         form = EnquiryForm(request.POST)
         if form.is_valid():
             enquiry = form.save(commit=False)
-            enquiry.user_profile = profile
+            if profile:
+                enquiry.user_profile = profile
             enquiry.save()
             messages.success(request, 'Enquiry submitted successfully')
             return redirect('enquiries')  
+
     else:
         form = EnquiryForm()
 
-    enquiries = profile.enquiries.all().order_by('-date')
+    if is_anonymous:
+        enquiries = Enquiry.objects.filter(enquiry_number__icontains=request.GET.get('enquiry_number', '')).order_by('-date')
+    else:
+        enquiries = profile.enquiries.all().order_by('-date')
 
     template = 'contactus/enquiries.html'
     context = {
         'enquiry_form': form,
         'profile': profile,
-        'on_profile_page': True,
+        'on_profile_page': is_anonymous,
         'enquiries': enquiries,
+        'is_anonymous': is_anonymous,
     }
 
     return render(request, template, context)
+
+
+def enquiry_detail(request, enquiry_id):
+    """ View for individual enquiry details """
+    enquiry = get_object_or_404(Enquiry, pk=enquiry_id)
+    
+    if request.user.is_authenticated:
+        profile = get_object_or_404(UserProfile, user=request.user)
+        if profile.enquiries.filter(id=enquiry.id).exists():
+            return render(request, 'contactus/enquiry_detail.html', {'enquiry': enquiry})
+        else:
+            messages.error(request, "You don't have permission to view this enquiry.")
+            return redirect('enquiries')
+    else:
+        messages.error(request, "You must be logged in to view enquiry details.")
+        return redirect('login')
