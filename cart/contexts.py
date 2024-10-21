349628f83c7a82edc71312500bee61d687108d5c
Warren Smith <warren.lee.smith@hotmail.co.uk>
2024-10-21 12:44:48 +0000

    UPDATE: Webhook handler to handle profiles + refactoring

--- cart/contexts.py
+++ cart/contexts.py
@@ -14,14 +14,14 @@ def cart_contents(request):
     for item_id, item_data in cart.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
-            total += item_data * product.price 
+            total += item_data * product.price
             product_count += item_data
             cart_items.append({
                 'item_id': item_id,
                 'quantity': item_data,
                 'product': product,
             })
-        else: 
+        else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
                 total += quantity * product.price
@@ -31,8 +31,7 @@ def cart_contents(request):
                     'quantity': quantity,
                     'product': product,
                     'size': size,
-            })
-    
+                })
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
