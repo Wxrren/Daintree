d6375e0c488f809c3c6655589003c18b0b8131f3
Warren Smith <warren.lee.smith@hotmail.co.uk>
2024-10-22 13:19:49 +0000

    FIX: Profile names not showing up. UPDATE: Refactoring urls

--- contactus/urls.py
+++ contactus/urls.py
@@ -1,12 +1,23 @@
 from django.urls import path
-from .views import enquiries, enquiry_detail, enquiries_success, superuser_enquiries
-from . import views
+from .views import (
+    enquiries, enquiry_detail,
+    enquiries_success, superuser_enquiries,
+    resolve_enquiry
+)
 
 urlpatterns = [
     path('enquiries/', enquiries, name='enquiries'),
-    path('enquiry-detail/<int:enquiry_id>/', enquiry_detail, name='enquiry-detail'),
-    path('enquiries-success/<int:pk>/', enquiries_success, name='enquiries_success'),
-    path('enquiry-detail/<int:enquiry_id>/', views.enquiry_detail, name='enquiry-detail'),
-    path('superuser-enquiries/', superuser_enquiries, name='superuser_enquiries'),
-    path('resolve/<int:enquiry_id>/', views.resolve_enquiry, name='resolve-enquiry'),
+    path(
+        'enquiry-detail/<int:enquiry_id>/',
+        enquiry_detail, name='enquiry-detail'
+    ),
+    path(
+        'enquiries-success/<int:pk>/',
+        enquiries_success, name='enquiries_success'
+    ),
+    path(
+        'superuser-enquiries/',
+        superuser_enquiries, name='superuser_enquiries'
+    ),
+    path('resolve/<int:enquiry_id>/', resolve_enquiry, name='resolve-enquiry'),
 ]
