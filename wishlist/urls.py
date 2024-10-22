4a7ef4564bdd2885bbbeceed7380a908dfc9a644
Warren Smith <warren.lee.smith@hotmail.co.uk>
2024-10-22 23:53:52 +0000

    ADD: Wishlist functionality

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.view_wishlist, name='view_wishlist'),
+    path('add/<item_id>/', views.add_to_wishlist, name='add_to_wishlist'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import UserProfile, Product, Wishlist
 from django.contrib import messages
-from .forms import WishlistForm
 
 
 def view_wishlist(request):
     """ A view that renders the wishlist contents page """
     
     return render(request, 'wishlist/wishlist.html')
+
+
+def add_to_wishlist(request, item_id):
+    """ Add a specified product to the wishlist """
+
+    product = get_object_or_404(Product, pk=item_id)
+    redirect_url = request.POST.get('redirect_url')
+    wishlist = request.session.get('wishlist', {})
+
+    if str(item_id) in wishlist:
+        messages.info(request, f"'{product.name}' is already in your wishlist.")
+    else:
+        wishlist[str(item_id)] = 1
+
+    request.session['wishlist'] = wishlist
+    print(request.session['wishlist'])
+    return redirect(redirect_url)
