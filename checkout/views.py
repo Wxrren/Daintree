a907b34c8666648a29951c5cdfe88db8b8f9f8c7
Warren Smith <warren.lee.smith@hotmail.co.uk>
2024-10-24 21:20:30 +0000

    REMOVE: Email settings

--- checkout/views.py
+++ checkout/views.py
@@ -62,7 +62,6 @@ def checkout(request):
             order.stripe_pid = pid
             order.original_cart = json.dumps(cart)
             order.save()
-
             for item_id, item_data in cart.items():
                 try:
                     product = Product.objects.get(id=item_id)
