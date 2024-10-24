d18b838f7cdc368cc8a62c393b9f68a3f020ad9b
Warren Smith <warren.lee.smith@hotmail.co.uk>
2024-10-24 09:13:28 +0000

    ADD: Hover JS - UPDATE: README

---
+++ static/js/base.js
@@ -0,0 +1,31 @@
+function contactHover() {
+    document.getElementById("contact-hover").style.backgroundColor = "white";
+}
+
+function contactNormal() {
+    document.getElementById("contact-hover").style.backgroundColor ="";
+}
+
+function accountHover() {
+    document.getElementById("account-hover").style.backgroundColor = "white";
+}
+
+function accountNormal() {
+    document.getElementById("account-hover").style.backgroundColor ="";
+}
+
+function cartHover() {
+    document.getElementById("cart-hover").style.backgroundColor = "white";
+}
+
+function cartNormal() {
+    document.getElementById("cart-hover").style.backgroundColor ="";
+}
+
+function productsHover() {
+    document.getElementById("products-hover").style.backgroundColor = "white";
+}
+
+function productsNormal() {
+    document.getElementById("products-hover").style.backgroundColor ="";
+}
